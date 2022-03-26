eae9a7df05ec7a979966c3bb1f22f5e11b5c28e3
prajvalchavan99 <prajvalchavan99@gmail.com>
2022-03-26 09:59:07 +0530

    update

--- weather/models.py
+++ weather/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class city(models.Model):
+    name = models.CharField(max_length=25)
+    latitude=models.CharField(max_length=10,null=True)
+    longitude=models.CharField(max_length=10,null=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'cities'
--- weather/views.py
+++ weather/views.py
@@ -2,22 +2,30 @@
 
 from django.shortcuts import render
 import requests
+from .models import city
 
 def index(request):
     context={}
-    url = 'https://api.weather.gov/gridpoints/TOP/31,80/forecast'
+    cities=city.objects.all()
+    weatherlist=[]
+    for data in cities:
+        url = 'https://api.weather.gov/points/'+ data.latitude +','+ data.longitude
 
-    city = 'Las Vegas'
-
-    city_weather = requests.get(url).json()
-    weather = {
-        'city' : city,
-        'temperature' : city_weather['properties']['periods'][0]['temperature'],
-        'format': city_weather['properties']['periods'][0]['temperatureUnit'],
-        'windspeed': city_weather['properties']['periods'][0]['windSpeed'],
-        'direction': city_weather['properties']['periods'][0]['windDirection'],
-        'description': city_weather['properties']['periods'][0]['detailedForecast'],
-    }
-    context['data']=weather
+        latlong = requests.get(url).json()
+        url2=latlong["properties"]["forecast"]
+        city_weather = requests.get(url2).json()
+        weather = {
+            'city' : data.name,
+            'temperature' : city_weather['properties']['periods'][0]['temperature'],
+            'format': city_weather['properties']['periods'][0]['temperatureUnit'],
+            'windspeed': city_weather['properties']['periods'][0]['windSpeed'],
+            'direction': city_weather['properties']['periods'][0]['windDirection'],
+            'description': city_weather['properties']['periods'][0]['detailedForecast'],
+            'latitude': data.latitude,
+            'longitude': data.longitude,
+        }
+        weatherlist.append(weather)
+    context['data']=weatherlist
+    context['length']=len(weatherlist)
     
     return render(request, 'index.html',context) 
